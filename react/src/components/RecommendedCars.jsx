0394a00798c0dc5bb0e2f5174508442ff9f8ea20
pixtonmr@gmail.com <wasadmin@example.com>
2024-07-16 12:11:32 -0400

    Started with Recommendations

--- react/src/components/RecommendedCars.jsx
+++ react/src/components/RecommendedCars.jsx
@@ -2,36 +2,41 @@
 
 import React from 'react';
 import { useState, useEffect } from "react";
+import { ListGroup, Row, Col } from 'react-bootstrap';
 
 const PORT_NUMBER = 5000;
-const END_POINT = `http://localhost:${PORT_NUMBER}/`;
+const END_POINT1 = `http://cors-anywhere.herokuapp.com/http://127.0.0.1:${PORT_NUMBER}`;
+const END_POINT2 = 'http://localhost:3000'
 
 const FetchResults = async (body, setData) => {
   try {
-    const response = await fetch(END_POINT + "api/predict", {
+    const response = await fetch(END_POINT1 + "/api/predict", {
       headers: {
         "Content-Type": "application/json",
       },
       method: "POST",
       body: JSON.stringify(body),
     });
+    
 
     if (!response.ok)
       throw new Error(`Response status: ${response.status}`);
 
     const json = await response.json();
+    console.log(json)
     setData(JSON.stringify(json));
+    console.log(data)
   } catch (e) {
     console.error("Error fetching from FetchResults: ", e);
   }
 };
 
-const FetchCar = async (data, setCars) => {
+const FetchCars = async (data, setCars) => {
   const json = {}
   for (let i = 0; i < 3; i++) {
     let carid = data[i]
     try {
-      const response = await fetch(END_POINT + `api/car/${carid}`, {
+      const response = await fetch(END_POINT2 + `/car/${carid}`, {
         headers: {
           "Content-Type": "application/json",
         },
@@ -55,10 +60,13 @@ const RecommendedCars = (props) => {
   const [cars, setCars] = useState('');
   FetchResults(props.car, setData)
   FetchCars(data, setCars)
+  console.log(cars)
+  console.log(data)
   return (
     <div>
-      <h4>Rocommended Cars</h4>
+      <h4>Recommended Cars</h4>
       <Row>
+        {console.log(cars)}
         {cars.map(car => (
           <Col key={car._id} xs={12} sm={6} md={4} className="mb-3">
             <ListGroup.Item>
